86aa5596cfe8f267a11db7acaeec2143175f6efd
jtimberman <joshua@chef.io>
2015-05-26 23:23:37 -0600

    add dhcp-leasefile (per platform)

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,8 @@
 default['dnsmasq']['dhcp'].tap do |dhcp|
-  dhcp['dhcp-range'] = "#{node['network']['default_interface']},10.13.37.100,10.13.37.254,12h"
-  dhcp['interface'] = node['network']['default_interface']
+  dhcp['dhcp-range']     = "#{node['network']['default_interface']},10.13.37.100,10.13.37.254,12h"
+  dhcp['interface']      = node['network']['default_interface']
+  dhcp['dhcp-leasefile'] = case node['platform_family']
+                           when 'rhel', 'fedora' then '/var/lib/dnsmasq/dnsmasq.leases'
+                           when 'debian' then '/var/lib/misc/dnsmasq.leases'
+                           end
 end
